1cb704ec8da853349b23247507c279020862ac46
Paul Solt <PaulSolt@gmail.com>
2020-01-27 12:32:30 -0500

    Added bridging header and model controller

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/LSIContactsController.m
@@ -0,0 +1,13 @@
+//
+//  LSIContactsController.m
+//  Contacts-Hybrid
+//
+//  Created by Paul Solt on 1/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIContactsController.h"
+
+@implementation LSIContactsController
+
+@end
