c97be52a582b1678ed582e47477abcb1f7e03bc8
Paul Solt <PaulSolt@gmail.com>
2020-05-18 12:15:40 -0400

    Added a new class.

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
@@ -0,0 +1,24 @@
+//
+//  Contact.swift
+//  Contacts-Hybrid
+//
+//  Created by Paul Solt on 5/18/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+// QUESTION: Why use a class in Swift?
+
+// To use this in Objective-C we need to subclass from NSObject
+// Swift Structs are not compatible with Objective-C structures
+
+class Contact: NSObject {
+    var name: String
+    var relationship: String?
+    
+    init(name: String, relationship: String?) {
+        self.name = name
+        self.relationship = relationship
+    }
+}
