3dc240990a5b424330dba9bb40f0007c600828dd
Paul Solt <PaulSolt@gmail.com>
2020-06-15 12:40:00 -0400

    Added Model Controller

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contacts-Hybrid-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// Project Files and Organization
+
+// If you move either of these files, you'll break the build
+// 1. Info.plist -> will give you warnings (strange errors about a 568 pixel launch image)
+// 2. Bridging header -> failure to compile
---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/LSIContactController.m
@@ -0,0 +1,13 @@
+//
+//  LSIContactController.m
+//  Contacts-Hybrid
+//
+//  Created by Paul Solt on 6/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIContactController.h"
+
+@implementation LSIContactController
+
+@end
